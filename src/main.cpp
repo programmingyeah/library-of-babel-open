2fbfde35bdc1fb56ca636f987e0cf1477e344b71
programmingyeah <ibnabidawud@gmail.com>
2025-12-11 21:28:36 +0100

    Minimal build + GLFW window display

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
-#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    if (!glfwInit()) return -1;
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Library of Babel", nullptr, nullptr);
+    if (!window) return -1;
+
+    glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
