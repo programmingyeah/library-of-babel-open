7f6d0063b09ea498f3173bf21f2bd14590f72da8
pencilvest <teainhaler@proton.me>
2025-12-18 10:46:26 +0100

    Added camera movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <GLFW/glfw3.h>
 #include <cstdint>
 #include <iostream>
 #include "renderer.hpp"
+#include "camera.hpp"
+
+
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -25,14 +32,26 @@ int main() {
         return -1;
     }
 
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+
+    Camera camera{};
     Renderer renderer{};
-    renderer.setup();
+    renderer.setup();    
+
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetWindowUserPointer(window, &camera);
+    glfwSetCursorPosCallback(window, camera.mouse_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     while (!glfwWindowShouldClose(window)) {
 
-        renderer.drawFrame(window);
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;  
+
+        camera.processInput(window, deltaTime);
+        renderer.drawFrame(window, cameraPos, cameraFront, cameraUp);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
